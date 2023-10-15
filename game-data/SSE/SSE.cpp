1963e5cdd6677f28d1464346c4a2cae734dc97e1
CHollingworth <->
2023-10-15 20:59:21 +0100

    Created SEE class.

---
+++ game-data/SSE/SSE.cpp
@@ -0,0 +1,10 @@
+//
+// Created by charles on 15/10/23.
+//
+
+#include "SSE.h"
+
+namespace Lamp {
+    namespace Game {
+    } // Lamp
+} // Game
---
+++ game-data/SSE/SSE.h
@@ -0,0 +1,18 @@
+//
+// Created by charles on 15/10/23.
+//
+
+#ifndef LAMP_SSE_H
+#define LAMP_SSE_H
+
+namespace Lamp {
+    namespace Game {
+
+        class SSE {
+
+        };
+
+    } // Lamp
+} // Game
+
+#endif //LAMP_SSE_H
