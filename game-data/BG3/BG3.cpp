e9ecb5c0f972375bf8c634df8f7537678946993e
Charles Hollingworth <72136789+CHollingworth@users.noreply.github.com>
2023-10-17 17:05:59 +0100

    Cyberpunk final (#54)

--- game-data/BG3/BG3.cpp
+++ game-data/BG3/BG3.cpp
@@ -59,7 +59,7 @@ Lamp::Game::lampReturn Lamp::Game::BG3::ConfigMenu() {
 Lamp::Game::lampReturn Lamp::Game::BG3::startDeployment() {
     Lamp::Core::lampControl::getInstance().inDeployment = true;
     if(KeyInfo()["installDirPath"] == "" | KeyInfo()["appDataPath"] == "" ) {
-        Core::Base::lampLog::getInstance().log("Game Configuration not set.", Core::Base::lampLog::warningLevel::WARNING, true, Core::Base::lampLog::LMP_NOCONFIG);
+        return Core::Base::lampLog::getInstance().pLog({0, "Game Configuration not set."}, Core::Base::lampLog::warningLevel::WARNING, true, Core::Base::lampLog::LMP_NOCONFIG);
     }
 
     Core::Base::LampSequencer::add("BG3 Deployment Queue", [this]() -> lampReturn {
